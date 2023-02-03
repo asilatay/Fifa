d220e1bd81a4bd308e445388a166c683361ecf33
asilatay <asilatay10@gmail.com>
2023-02-03 15:17:33 +0300

    spring boot run

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world Fifa-Mandalin-Zandalin");
+        SpringApplication.run(Main.class, args);
     }
 }
